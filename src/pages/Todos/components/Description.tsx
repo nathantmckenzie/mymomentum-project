33aa94454c58f8dfe79f62ea567f17905683f889
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-04-17 15:48:19 -0700

    Improving UI of the Description page

--- src/pages/Todos/components/Description.tsx
+++ src/pages/Todos/components/Description.tsx
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import {useUpdateToDoMutation} from "../../../generated/graphql";
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 export default function Description({ todo }) {
-    const [input, setInput] = useState("")
+    const [input, setInput] = useState("");
     const [description, setDescription] = useState(todo.description);
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          '& > *': {
+            margin: theme.spacing(1),
+          },
+        },
+        descriptionContainer: {
+          display: "flex",
+          marginTop: "20px"
+        },
+        submitButton: {
+          marginLeft: "30px"
+        },
+        description: {
+          marginTop: "30px"
+        }
+      }));
+
+    const classes = useStyles();
 
     const [updateToDoMutation, { data, loading, error }] = useUpdateToDoMutation({
         variables: {
@@ -28,12 +51,14 @@ export default function Description({ todo }) {
 
     return (
         <div>
-          Description:
+          <div className={classes.descriptionContainer} >
+            <TextField value={input} onChange={(e) => setInput(e.target.value)} label="Add Description" />
+            <Button className={classes.submitButton} variant="contained" onClick={onSubmit}>Submit</Button>
+          </div>
           <br />
-          <textarea value={input} onChange={(e) => setInput(e.target.value)} />
-          <button onClick={onSubmit}>Submit</button>
-          <br />
-          {description}
+          <div className={classes.description}>
+          Description: {description}
+          </div>
         </div>
     )
 }
